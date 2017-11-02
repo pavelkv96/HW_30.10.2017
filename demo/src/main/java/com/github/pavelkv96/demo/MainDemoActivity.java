2ce6137bbfb862c8356755d03c52cefed8666a6b
pavelkv96 <pavelkv96@gmail.com>
2017-11-02 22:31:27 +0300

    Initial UI and Demo modules

---
+++ demo/src/main/java/com/github/pavelkv96/demo/MainDemoActivity.java
@@ -0,0 +1,17 @@
+package com.github.pavelkv96.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Pavel on 02.11.2017.
+ */
+
+public class MainDemoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
